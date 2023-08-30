6a7caf63af995f617b299fc68fcd47164650b80f
ganeshkale07 <ggkale07@gmail.com>
2023-08-30 09:56:57 +0530

    string type and selector assignment

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 
 import { AuthService } from './auth.service';
 import { Store } from '@ngrx/store';
+import { State } from '../state/app.state';
+import { getMaskerUserName } from './state/user.reducer';
 
 @Component({
   templateUrl: './login.component.html',
@@ -17,12 +19,12 @@ export class LoginComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private router: Router,
-    private store: Store<any>
+    private store: Store<State>
   ) {}
 
   ngOnInit(): void {
-    this.store.select('User').subscribe((user) => {
-      this.maskUserName = user.maskUserName;
+    this.store.select(getMaskerUserName).subscribe((maskUserName) => {
+      this.maskUserName = maskUserName;
     });
   }
 
--- src/app/user/state/user.reducer.ts
+++ src/app/user/state/user.reducer.ts
@@ -1,11 +1,44 @@
-import { createAction, createReducer , on} from "@ngrx/store";
+import {
+  createAction,
+  createFeatureSelector,
+  createReducer,
+  createSelector,
+  on,
+} from '@ngrx/store';
 
-export const maskUserNameReducer = createReducer(
-    {maskUserName : false},
-    on(createAction('[maskUserName] Toggle mask user'), (state) => {
-        return {
-        ...state,
-        maskUserName : !state.maskUserName
-        }
-    })
+interface UserState {
+  maskUserName: boolean;
+  currentUser: string;
+  password: string;
+}
+
+const initialUserState: UserState = {
+  maskUserName: null,
+  password: null,
+  currentUser: null,
+};
+
+//Creating a selector
+const getuserFeatureSelector = createFeatureSelector<UserState>('User');
+
+//create selector for maskerUsername flag
+export const getMaskerUserName = createSelector(
+    getuserFeatureSelector,
+  (user) => user.maskUserName
+);
+
+//create selector for currentUser flag
+export const getCurrentUser = createSelector(
+    getuserFeatureSelector,
+  (user) => user.currentUser
+);
+
+export const maskUserNameReducer = createReducer<UserState>(
+  initialUserState,
+  on(createAction('[maskUserName] Toggle mask user'), (state) => {
+    return {
+      ...state,
+      maskUserName: !state.maskUserName,
+    };
+  })
 );
